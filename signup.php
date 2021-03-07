590a18a341675eaa9d4ae898366c6b6e69126cb9
Mohammed Sibahi <sibahi@live.com>
2021-03-07 05:49:24 +0200

    edit the style of the sign up and login pages

--- signup.php
+++ signup.php
@@ -37,27 +37,32 @@
 			</div>
 			<div class="sign-up-htm">
 				<div class="group">
-					<label for="user" class="label">Username</label>
-					<input id="user" type="text" class="input">
+					
+					<input id="user" type="text" class="input" placeholder="Full Name ...">
 				</div>
-				<div class="group">
-					<label for="pass" class="label">Password</label>
-					<input id="pass" type="password" class="input" data-type="password">
+                <div class="group">
+					
+					<input id="user" type="text" class="input" placeholder="Username ...">
 				</div>
-				<div class="group">
-					<label for="pass" class="label">Repeat Password</label>
-					<input id="pass" type="password" class="input" data-type="password">               
-                </div>
-				<div class="group">
-					<label for="pass" class="label">Email Address</label>
-					<input id="pass" type="text" class="input">
+                <div class="group">
+					
+					<input id="pass" type="text" class="input" placeholder="Email Address ...">
 	                </div>
+				<div class="group">
+					
+					<input id="pass" type="password" class="input" data-type="password" placeholder="Password ...">
+				</div>
+				<div class="group">
+					
+					<input id="pass" type="password" class="input" data-type="password" placeholder="Repeat Password ...">               
+                </div>
+				
 				<div class="group">
 					<input type="submit" class="button" value="Sign Up">
 				</div>
 				<div class="hr"></div>
 				<div class="foot-lnk">
-					<label for="tab-1">Already Member?</a>
+					<label for="tab-1">Already A Member?</a>
 				</div>
 			</div>
 		</div>
