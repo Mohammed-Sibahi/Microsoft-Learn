80ab68897db94656ed7c79f89e8aa129dba05ae5
Mohammed Sibahi <sibahi@live.com>
2021-03-09 02:04:42 +0200

    add the includes folder and files

---
+++ includes/dbh.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+$serverName = "localhost";
+
+$dBUsername = "root";
+
+$dBPassword = "";
+
+$dBName = "mircosoftlearn";
+
+
+/*we define a variable $conn and use a mysqli function (mysqli_connect) to make the connection using 
+the servername, username, password, and databse name   */
+$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+
+
+// if the connection fails, a message will show indicating a failed connection and the number of error. 
+if (!$conn) {
+
+    die("connection failed: " . mysqli_connect_error());
+}
--- signup.php
+++ signup.php
@@ -1,73 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles-2.css" >
-    <title>Document</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="styles-2.css">
+	<title>Document</title>
 </head>
+
 <body>
 
-<div class="login-wrap">
-	<div class="login-html">
-		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
-		<input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
-		<div class="login-form">
-			<div class="sign-in-htm">
-				<div class="group">
-					<label for="user" class="label">Username</label>
-					<input id="user" type="text" class="input">
+	<div class="login-wrap">
+		<div class="login-html">
+			<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Log In</label>
+			<input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
+			<div class="login-form">
+				<div class="sign-in-htm">
+					<form action="login.inc.php" method="POST">
+						<div class="group">
+							
+							<input id="user" type="text" class="input" placeholder="Username / Email">
+						</div>
+						<div class="group">
+							
+							<input id="pass" type="password" class="input" data-type="password" placeholder="Password">
+						</div>
+						<div class="group">
+							<input id="check" type="checkbox" class="check" checked>
+							<label for="check"><span class="icon"></span> Keep me Signed in</label>
+						</div>
+						<div class="group">
+							<input type="submit" class="button" value="Log In">
+						</div> 
+						<div class="hr"></div>
+						<div class="foot-lnk">
+							<a href="#forgot">Forgot Password?</a>
+						</div>
+					</form>
 				</div>
-				<div class="group">
-					<label for="pass" class="label">Password</label>
-					<input id="pass" type="password" class="input" data-type="password">
-				</div>
-				<div class="group">
-					<input id="check" type="checkbox" class="check" checked>
-					<label for="check"><span class="icon"></span> Keep me Signed in</label>
-				</div>
-				<div class="group">
-					<input type="submit" class="button" value="Sign In">
-				</div>
-				<div class="hr"></div>
-				<div class="foot-lnk">
-					<a href="#forgot">Forgot Password?</a>
-				</div>
-			</div>
-			<div class="sign-up-htm">
-				<div class="group">
-					
-					<input id="user" type="text" class="input" placeholder="Full Name ...">
-				</div>
-                <div class="group">
-					
-					<input id="user" type="text" class="input" placeholder="Username ...">
-				</div>
-                <div class="group">
-					
-					<input id="pass" type="text" class="input" placeholder="Email Address ...">
-	                </div>
-				<div class="group">
-					
-					<input id="pass" type="password" class="input" data-type="password" placeholder="Password ...">
-				</div>
-				<div class="group">
-					
-					<input id="pass" type="password" class="input" data-type="password" placeholder="Repeat Password ...">               
-                </div>
-				
-				<div class="group">
-					<input type="submit" class="button" value="Sign Up">
-				</div>
-				<div class="hr"></div>
-				<div class="foot-lnk">
-					<label for="tab-1">Already A Member?</a>
+				<div class="sign-up-htm">
+
+					<form action="signup.inc.php" method="POST">
+						<div class="group">
+							<input id="user" type="text" name="name" class="input" placeholder="Full Name ...">
+						</div>
+						<div class="group">
+
+							<input id="user" type="text" name="uid" class="input" placeholder="Username ...">
+						</div>
+						<div class="group">
+
+							<input id="pass" type="text" name="email" class="input" placeholder="Email Address ...">
+						</div>
+						<div class="group">
+
+							<input id="pass" type="password" name="pwd" class="input" data-type="password" placeholder="Password ...">
+						</div>
+						<div class="group">
+
+							<input id="pass" type="password" name="pwdrepeat" class="input" data-type="password" placeholder="Repeat Password ...">
+						</div>
+
+						<div class="group">
+							<input type="submit" class="button" name="submit" value="Sign Up">
+						</div>
+						<div class="hr"></div>
+						<div class="foot-lnk">
+							<label for="tab-1">Already A Member?</a>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
 
 </body>
+
 </html>
