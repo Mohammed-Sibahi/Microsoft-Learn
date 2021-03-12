8233c803aadb9958acc0c76c4c3057ccff15deb8
Mohammed Sibahi <sibahi@live.com>
2021-03-12 09:50:02 +0200

    write the login code

---
+++ includes/login.inc.php
@@ -0,0 +1,26 @@
+<?php
+
+if (isset($_POST["submit"])) {
+
+    $username = $_POST["uid"];
+
+    $pwd = $_POST["pwd"];
+
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
+
+    if(emptyInputLogin($username, $pwd) !== false) {
+
+        header("location: ../signup.php?error=emptyinput");
+        exit();
+    }
+
+    loginUser($conn, $username, $pwd);
+
+
+}
+
+else {
+    header("location: ../signup.php");
+    exit();
+}
